58f0a3452f6d7d085a02e0c2dd68f8ede0375f54
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-07-17 10:23:12 +0600

    condition impliment

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ app.get('/news/:id', (req, res) => {
     res.send(selectedNews);
 })
 
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews);
+    }
+})
+
 app.listen(port, () => {
     console.log(`News running port: ${port}`);
 })
